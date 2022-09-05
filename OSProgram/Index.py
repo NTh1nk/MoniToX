28935069a6132ff57475894772d1091cd09e2488
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2022-09-05 19:18:17 +0200

    Update Index.py

--- OSProgram/Index.py
+++ OSProgram/Index.py
@@ -1,9 +1,18 @@
+from struct import pack
 import pyshark
 capture = pyshark.LiveCapture(interface='WiFi')
 ip = {}
 os = {}
-for packet in capture.sniff_continuously(packet_count=5):
-    if packet.ip.src not in ip:
-        ip[packet.ip.src] = 0
-    ip[packet.ip.src] += 1
-print(ip)
+for packet in capture.sniff_continuously(packet_count=25):
+    if "IP" in packet:
+        if packet.ip.src not in ip:
+            ip[packet.ip.src] = 0      
+        ip[packet.ip.src] += 1
+    if "SSDP" in packet:
+        print(packet.ssdp.field_names)
+        if packet.ssdp.http_user_agent not in os:
+            os[packet.ssdp.http_user_agent] = 0      
+        os[packet.ssdp.http_user_agent] += 1
+
+       
+print(ip, os)
