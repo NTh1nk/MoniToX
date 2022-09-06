39249f6c9b4c1e1e8dc197632756e18e88540864
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2022-09-06 16:52:20 +0200

    Update ChartTest.py

--- OSProgram/ChartTest.py
+++ OSProgram/ChartTest.py
@@ -1,16 +1,18 @@
+import string
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-
+keys = ["Windows", "Mac "]
 os = {"Windows":3,"Mac":5}
-osdata = []
 osname = []
-osname = os.items(os["Mac","Windows"])
-osdata = os.items(os["Mac"])
+osdata = [2,5]
+
+
 
  
 fig = plt.figure(figsize =(10, 7))
-plt.pie(osdata, labels = osname)
+
+plt.pie(osdata, labels = keys)
        
 plt.show()  
