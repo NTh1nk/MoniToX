5d4e31ba01aaf75a99e9150b2e212b64eb8e17dd
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2022-09-12 18:02:19 +0200

    IT WORKS

--- OSProgram/ThreadingTest.py
+++ OSProgram/ThreadingTest.py
@@ -12,24 +12,24 @@ capture = pyshark.LiveCapture(interface='WiFi')
 def Check_Packets():
     global os
     global ip
-    for i in range(1):
+    while True:
         for packet in capture.sniff_continuously(packet_count=25):
             if "IP" in packet:
                 if packet.ip.src not in ip:
                     ip[packet.ip.src] = 0      
-            ip[packet.ip.src] += 1
+                ip[packet.ip.src] += 1
             if "SSDP" in packet:
                 if packet.ssdp.http_user_agent not in os:   
                     os[packet.ssdp.http_user_agent] = 0      
                 os[packet.ssdp.http_user_agent] += 1
-
+        print(ip, os)
 
 def CreateGraph ():
     global os
     global ip
     plt.pie(os.values(), labels = os.keys(), colors=["k", "b", "g", "r","c"]) 
     plt.pause(0.05)  
-print(ip, os)
+
 threads = []
 fig = plt.figure(figsize =(10, 7))
 plt.ion()
